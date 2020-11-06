d75447906dba07e64a1e69d0ca598e61bc42230a
Riley Lai <dakimi07@gmail.com>
2020-11-06 21:13:49 +0800

    Feat: show movie list
    - Using handlebars to create templates.
    - Include datas of movies and show them through handlebars helpers.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const movieList = require('./movies.json')
 
 // Define server related variables
 const port = 3000
@@ -15,15 +16,16 @@ app.use(express.static('public'))
 
 // Handle request and response
 app.get('/', (req, res) => {
-  const movieOne = {
-    id: 1,
-    img: 'https://movie-list.alphacamp.io/posters/c9XxwwhPHdaImA2f1WEfEsbhaFB.jpg',
-    title: 'Jurassic World: Fallen Kingdom'
-  }
-  res.render('index', { movie: movieOne })
+  res.render('index', { movies: movieList.results })
+})
+
+app.get('/movies/:movie_id', (req, res) => {
+  const movieID = req.params.movie_id
+  const movie = movieList.results.find(item => item.id === Number(movieID))
+  res.render('show', { movie: movie })
 })
 
 // Start and listen to the server
 app.listen(port, () => {
-  console.log(`The server is listening on http://localhost:${port}`)
+  console.log(`The server is listening on http://localhost:${port}`);
 })
